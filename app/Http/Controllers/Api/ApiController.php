aab09c3c375c67722749af7d5fc1ffadcabe1b84
Tawsif Aqib <tawsif.aqib@gmail.com>
2017-05-29 01:56:41 +0600

    move code from RestApiController trait to ApiController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Traits\RestApiController;
+use Illuminate\Http\Request;
 use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
-    use Helpers, RestApiController;
+    use Helpers;
 
     /**
      * Model repository.
@@ -23,4 +23,94 @@ class ApiController extends Controller
      * @var bool
      */
     protected $paginate = true;
+
+    /**
+     * Show all items of a resource.
+     *
+     * @return \Dingo\Api\Http\Response
+     */
+    public function index()
+    {
+        $perPage = (int) app('request')->input('per_page')
+            ?: config('repository.pagination.limit');
+
+        if ($this->paginate == false || app('request')->input('per_page') == -1) {
+            return $this->response->array($this->repository->all());
+        }
+
+        return $this->response->array($this->repository->paginate($perPage));
+    }
+
+    /**
+     * Store a new resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Dingo\Api\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->repository->rules());
+
+        $this->repository->create($request);
+
+        return $this->response->created();
+    }
+
+    /**
+     * Show a single resource.
+     *
+     * @param int $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function show(int $id)
+    {
+        return $this->response->array($this->repository->find($id));
+    }
+
+
+    /**
+     * Update a single resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function update(Request $request, int $id)
+    {
+        $this->validate($request, $this->repository->rules());
+
+        $this->repository->update($this->getRequestValues($request), $id);
+
+        return $this->response->accepted();
+    }
+
+    /**
+     * Delete a single resource.
+     *
+     * @param int $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        $this->repository->delete($id);
+
+        return $this->response->noContent();
+    }
+
+    /**
+     * Get the request input values.
+     * - Get the JSON values first
+     * - Get the Form data values if JSON is not available
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    private function getRequestValues(Request $request)
+    {
+        if ($request->json()->all()) {
+            return $request->json()->all();
+        }
+
+        return $request->all();
+    }
 }
